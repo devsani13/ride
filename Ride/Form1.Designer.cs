89dd78a737d2edcd9d4996e739c5cf9fb89dfee4
Sani <samuelmartinspassos@gmail.com>
2025-02-03 01:24:01 -0300

    Commit 030225

---
+++ Ride/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Ride
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtpontos = new System.Windows.Forms.Label();
+            this.cima = new System.Windows.Forms.PictureBox();
+            this.baixo = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cima)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baixo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Ride.Properties.Resources.player2;
+            this.player.Location = new System.Drawing.Point(12, 107);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(88, 45);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Tag = "player";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Ride.Properties.Resources.bot11;
+            this.pictureBox2.Location = new System.Drawing.Point(360, 107);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(92, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "bot";
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Ride.Properties.Resources.bot24;
+            this.pictureBox3.Location = new System.Drawing.Point(169, 107);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(92, 54);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "bot";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.Timer);
+            // 
+            // txtpontos
+            // 
+            this.txtpontos.AutoSize = true;
+            this.txtpontos.BackColor = System.Drawing.Color.Transparent;
+            this.txtpontos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpontos.ForeColor = System.Drawing.Color.White;
+            this.txtpontos.Location = new System.Drawing.Point(715, 9);
+            this.txtpontos.Name = "txtpontos";
+            this.txtpontos.Size = new System.Drawing.Size(85, 20);
+            this.txtpontos.TabIndex = 12;
+            this.txtpontos.Text = "Pontos: 0";
+            // 
+            // cima
+            // 
+            this.cima.Location = new System.Drawing.Point(267, 130);
+            this.cima.Name = "cima";
+            this.cima.Size = new System.Drawing.Size(26, 22);
+            this.cima.TabIndex = 13;
+            this.cima.TabStop = false;
+            this.cima.Visible = false;
+            this.cima.Click += new System.EventHandler(this.cima_Click);
+            // 
+            // baixo
+            // 
+            this.baixo.Location = new System.Drawing.Point(299, 130);
+            this.baixo.Name = "baixo";
+            this.baixo.Size = new System.Drawing.Size(26, 22);
+            this.baixo.TabIndex = 14;
+            this.baixo.TabStop = false;
+            this.baixo.Visible = false;
+            this.baixo.Click += new System.EventHandler(this.baixo_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Ride.Properties.Resources.bot24;
+            this.pictureBox1.Location = new System.Drawing.Point(527, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "bot";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::Ride.Properties.Resources.bot11;
+            this.pictureBox4.Location = new System.Drawing.Point(665, 107);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(92, 45);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "bot";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Ride.Properties.Resources.road;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(812, 270);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.baixo);
+            this.Controls.Add(this.cima);
+            this.Controls.Add(this.txtpontos);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(828, 309);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(828, 309);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cima)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baixo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label txtpontos;
+        private System.Windows.Forms.PictureBox cima;
+        private System.Windows.Forms.PictureBox baixo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Ride/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ride
+{
+    public partial class Form1 : Form
+    {
+        int vbots = 10;
+        int pontos = 0;
+        Random rnd = new Random();
+        int posicao;
+        bool perdeu = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Reiniciar();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Misturar()
+        {
+            perdeu = false;
+            txtpontos.Text = "Pontos: " + pontos;
+            player.Image = Properties.Resources.player;
+
+            foreach (Control y in this.Controls)
+            {
+                if (y is PictureBox && (string)y.Tag == "bot")
+                {
+                    posicao = this.ClientSize.Height + rnd.Next(-900, -600) + (y.Height * 13);
+                    y.Top = posicao;
+                }
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "bot")
+                {
+                    posicao = this.ClientSize.Width + rnd.Next(500, 800) + (x.Width * 10);
+                    x.Left = posicao;
+                }
+            }
+        }
+
+        private void Reiniciar()
+        {
+            motor();
+            vbots = 10;
+            pontos = 0;
+            perdeu = false;
+            txtpontos.Text = "Pontos: " + pontos;
+            player.Image = Properties.Resources.player;
+            player.Top = 107;
+
+
+            foreach (Control y in this.Controls)
+            {
+                if (y is PictureBox && (string)y.Tag == "bot")
+                {
+                    posicao = this.ClientSize.Height + rnd.Next(-900, -600) + (y.Height * 13);
+                    y.Top = posicao;
+                }
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "bot")
+                {
+                    posicao = this.ClientSize.Width + rnd.Next(500, 800) + (x.Width * 10);
+                    x.Left = posicao;
+                }
+            }
+
+            timer1.Start();
+        }
+
+        private void Timer(object sender, EventArgs e)
+        {
+            txtpontos.Text = "Pontos: " + pontos;
+
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "bot")
+                {
+                    x.Left -= vbots;
+
+                    if (x.Left < -250)
+                    {
+                        x.Left = this.ClientSize.Width + rnd.Next(200, 500) + (x.Width * 15);
+                        Misturar();
+                        pontos += 4;
+
+                        if (pontos == 100)
+                        {
+                            timer1.Stop();
+                            MessageBox.Show("Parabéns, você atingiu os 100 pontos!");
+                            Reiniciar();
+                        }
+                    }
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        timer1.Stop();
+                        txtpontos.Text += " Pressione R para reiniciar o jogo";
+                        perdeu = true;
+                    }
+                }
+            }
+
+            if (pontos > 5)
+            {
+                vbots = 15;
+            }
+            if (pontos > 15)
+            {
+                vbots = 25;
+            }
+            if (pontos > 25)
+            {
+                vbots = 35;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                cima_Click(sender,e);
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                baixo_Click(sender, e);
+            }
+        }
+
+        private void cima_Click(object sender, EventArgs e)
+        {
+            if (player.Top > 0)
+            {
+                player.Top = player.Top - 17;
+            }
+        }
+
+        private void baixo_Click(object sender, EventArgs e)
+        {
+            if (player.Top < 99999999999999999)
+            {
+                player.Top = player.Top + 17;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R && perdeu == true)
+            {
+                Reiniciar();
+            }
+            if (e.KeyCode == Keys.H && perdeu == false)
+            {
+                motor();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
